0577f39ea088e399f533b84152d2e3e385231504
Vinayak-Sharma-744 <sharmavinayak744@gmail.com>
2023-03-19 22:52:46 +0530

    Instagram Clone

---
+++ script.js
@@ -0,0 +1,15 @@
+const profile = document.getElementById("PP")
+let menu = document.getElementById("menu")
+
+profile.addEventListener('click', fun)
+
+function fun(){
+    if (menu.style.display === "none") {
+        menu.style.display = "block"
+        console.log("check1")
+    } 
+    else {
+        menu.style.display = "none"
+        console.log("check2")
+    }
+}
